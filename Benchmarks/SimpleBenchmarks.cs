f84e67be595e46070a0b3c8bb07896bafe84f8de
Alexander Kayed <alexander.kayed@noser.com>
2022-08-22 11:01:24 +0200

    Added simple benchmarks

---
+++ Benchmarks/SimpleBenchmarks.cs
@@ -0,0 +1,20 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks;
+
+[JsonExporterAttribute.Full]
+[JsonExporterAttribute.FullCompressed]
+public class SimpleBenchmarks
+{
+    [Benchmark]
+    public void Wait50()
+    {
+        Thread.Sleep(50);
+    }
+
+    [Benchmark]
+    public void Wait200()
+    {
+        Thread.Sleep(200);
+    }
+}
