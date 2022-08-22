38575dd5be50f57fc972f090bb3fa84b78670048
Alexander Kayed <alexander.kayed@noser.com>
2022-08-22 13:42:52 +0200

    Reset timings

--- Benchmarks/SimpleBenchmarks.cs
+++ Benchmarks/SimpleBenchmarks.cs
@@ -9,12 +9,12 @@ public class SimpleBenchmarks
     [Benchmark]
     public void Wait50()
     {
-        Thread.Sleep(80);
+        Thread.Sleep(50);
     }
 
     [Benchmark]
     public void Wait200()
     {
-        Thread.Sleep(250);
+        Thread.Sleep(200);
     }
 }
