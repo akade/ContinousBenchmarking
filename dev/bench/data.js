576b5a867e8ae99e5bf5113896dcda227f0c9eda
github-action-benchmark <github@users.noreply.github.com>
2022-08-22 09:39:03 +0000

    add Benchmark (benchmarkdotnet) benchmark result for be5a876530e1bce6e31bb61b0d7ef7c85eaf8cb1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1661160797246,
+  "lastUpdate": 1661161143986,
   "repoUrl": "https://github.com/akade/ContinousBenchmarking",
   "entries": {
     "Benchmark": [
@@ -36,6 +36,42 @@ window.BENCHMARK_DATA = {
             "value": 200159073.64444444,
             "unit": "ns",
             "range": "± 32977.454759578104"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "16369295+akade@users.noreply.github.com",
+            "name": "Alex",
+            "username": "akade"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "be5a876530e1bce6e31bb61b0d7ef7c85eaf8cb1",
+          "message": "Update benchmarks.yml",
+          "timestamp": "2022-08-22T11:37:59+02:00",
+          "tree_id": "65953345c290d8c25b323dfd53afcbfd652f46e5",
+          "url": "https://github.com/akade/ContinousBenchmarking/commit/be5a876530e1bce6e31bb61b0d7ef7c85eaf8cb1"
+        },
+        "date": 1661161143381,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Benchmarks.SimpleBenchmarks.Wait50",
+            "value": 50080814.67333334,
+            "unit": "ns",
+            "range": "± 3457.614622720149"
+          },
+          {
+            "name": "Benchmarks.SimpleBenchmarks.Wait200",
+            "value": 200088761.13333333,
+            "unit": "ns",
+            "range": "± 5696.521677218832"
           }
         ]
       }
