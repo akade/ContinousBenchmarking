488a2d1983333e60daf3a6347c4f365005f85f2b
Alexander Kayed <alexander.kayed@noser.com>
2022-08-22 11:47:39 +0200

    Changed timings again

--- Benchmarks/SimpleBenchmarks.cs
+++ Benchmarks/SimpleBenchmarks.cs
@@ -9,12 +9,12 @@ public class SimpleBenchmarks
     [Benchmark]
     public void Wait50()
     {
-        Thread.Sleep(30);
+        Thread.Sleep(80);
     }
 
     [Benchmark]
     public void Wait200()
     {
-        Thread.Sleep(300);
+        Thread.Sleep(250);
     }
 }
